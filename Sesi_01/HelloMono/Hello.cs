47e5991c9d3565f053ebb296ee4b5bc9714548bf
mhmmdfkrazr <89122220+mhmmdfkrazr@users.noreply.github.com>
2021-11-22 14:09:11 +0700

    Belajar C#

---
+++ Sesi_01/HelloMono/Hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloMono
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nama Saya Fikri");
+        }
+    }
+}
