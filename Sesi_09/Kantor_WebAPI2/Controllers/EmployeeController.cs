9cabbf812483f821d4a66bc94b5ee18e96e2ed3a
mhmmdfkrazr <89122220+mhmmdfkrazr@users.noreply.github.com>
2021-11-26 13:13:41 +0700

    Sesi 9 Tinggal tugas

---
+++ Sesi_09/Kantor_WebAPI2/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Kantor_WebAPI2.Models;
+
+namespace Kantor_WebAPI2.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private EmployeeContext _context;
+        public EmployeeController(EmployeeContext context)
+        {
+            this._context = context;
+        }
+
+        public ActionResult<IEnumerable<EmployeeItem>> GetEmployeeItem()
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(EmployeeContext)) as EmployeeContext;
+            return _context.GetAllEmployee();
+        }
+    }
+}
